ca624311d258f5bf0567462cac0edb5e86a1f47e
Jack <>
2023-03-15 13:44:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let divtitle=document.querySelector(".title");
+
+let divopening=document.querySelector(".story-opening");
+
+let divbutton=document.querySelector(".buttons");
+
+let divoption1=document.querySelector(".option-one-screen"”);
+
+let divoption2=document.querySelector(".option-two-screen");
+
+let div1end=document.querySelector(".optiononeend");
+
+let div2end=document.querySelector(".option-two-end");
+
+
+
+let buttontag=document.querySelector(".option-one");
+
+let buttontag=document.querySelector(".option-two");
+
+
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
